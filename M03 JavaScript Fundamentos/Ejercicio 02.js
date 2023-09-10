38d18adfe75629b748402edd491316b9bc5e16ff
jarold ruda <jarold1984@gmail.com>
2023-09-10 06:52:01 -0500

    Resuelve y verifica test ejercicios 1 y 2

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var operacion = x + y
+   return operacion;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var operacion = x - y;
+   return operacion;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var operacion = x / y;
+   return operacion;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var operacion = x * y;
+   return operacion;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var operacion = x % y;
+   return operacion;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
